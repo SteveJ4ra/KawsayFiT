98a4bf076fc93ba72297de33a3586ff59a05af06
TheGreatKamus <emiliofloresga@gmail.com>
2025-07-15 14:05:16 -0500

    Login actualizado

--- src/main/java/edu/unl/cc/kawsayfit/controller/security/LoginBean.java
+++ src/main/java/edu/unl/cc/kawsayfit/controller/security/LoginBean.java
@@ -1,6 +1,11 @@
 package edu.unl.cc.kawsayfit.controller.security;
 
+import edu.unl.cc.kawsayfit.model.User;
+import edu.unl.cc.kawsayfit.service.UserService;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,7 +17,10 @@ import java.util.logging.Logger;
 @RequestScoped
 public class LoginBean implements Serializable {
 
-    private static Logger logger = Logger.getLogger(LoginBean.class.getName());
+    private static final Logger logger = Logger.getLogger(LoginBean.class.getName());
+
+    @Inject
+    private UserService userService;
 
     @NotNull
     private String email;
@@ -21,32 +29,38 @@ public class LoginBean implements Serializable {
     @Size(min=8, message="Contraseña muy corta")
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    //metodos
-
-    public String login(){
-        logger.info("Logging in with username: " + email);
-        logger.info("Logging in with password: " + password);
+    // Método principal de login
+    public String login() {
+        logger.info("Intentando iniciar sesión con: " + email);
 
         try {
+            User user = userService.validateUser(email, password);
+
+            if (user != null) {
+                FacesContext.getCurrentInstance()
+                        .getExternalContext()
+                        .getSessionMap()
+                        .put("currentUser", user);
+
+                return "/dashboard.xhtml?faces-redirect=true";
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Correo o contraseña incorrectos", null));
+                return null;
+            }
         } catch (Exception e) {
+            logger.severe("Error al iniciar sesión: " + e.getMessage());
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Error inesperado al iniciar sesión", null));
             return null;
         }
-        return "index";
     }
 }
--- src/main/java/edu/unl/cc/kawsayfit/service/UserService.java
+++ src/main/java/edu/unl/cc/kawsayfit/service/UserService.java
@@ -1,4 +1,9 @@
 package edu.unl.cc.kawsayfit.service;
 
+import edu.unl.cc.kawsayfit.model.User;
+
 public class UserService {
+    public User validateUser(String email, String password) {
+        return null; //TODO
+    }
 }
