2471f460acd7137ae9c93a6cefe990884837d4e2
Steven Jumbo <stevenalexanderjumbojaramillo@gmail.com>
2025-07-20 17:16:58 -0500

    corrigiendo BD

---
+++ src/main/java/edu/unl/cc/kawsayfit/business/Dish.java
@@ -0,0 +1,4 @@
+package edu.unl.cc.kawsayfit.business;
+
+public class Dish {
+}
---
+++ src/main/resources/META-INF/initial-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO dishes (name, description, imagePath, ingredients, caloriesPerServing, proteins, carbohydrates, fats)
+VALUES
+    ('AguadoGallina', 'Sopa tradicional con gallina, arroz y verduras', 'images/AguadoGallina.png', 'Gallina, arroz, cebolla, ajo, zanahoria', 300, 25, 20, 10),
+    ('AlitasAsadas', 'Alitas de pollo sazonadas y asadas al horno', 'images/AlitasAsadas.png', 'Alitas de pollo, especias, sal, aceite', 420, 35, 5, 30),
+    ('ArrozConLeche', 'Postre dulce a base de arroz y leche', 'images/ArrozConLeche.png', 'Arroz, leche, azúcar, canela', 200, 6, 35, 4),
+    ('CevicheCamaron', 'Plato frío típico con camarones y limón', 'images/CevicheCamaron.png', 'Camarones, limón, cebolla, tomate, cilantro', 250, 20, 10, 8),
+    ('Chaulafan', 'Arroz frito estilo oriental con carne y vegetales', 'images/Chaulafan.png', 'Arroz, cerdo, pollo, camarones, huevo, cebolla', 600, 30, 50, 20),
+    ('Encebollado', 'Plato caliente de pescado con yuca y cebolla', 'images/Encebollado.png', 'Pescado, yuca, cebolla, tomate, limón', 350, 28, 18, 12);
